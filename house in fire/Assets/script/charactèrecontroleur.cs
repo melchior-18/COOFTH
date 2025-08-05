92e817bbb20169b90857a941d3135a99d4deb8a2
Melchior <melc18hior@gmail.com>
2025-08-05 13:37:12 +0200

    imported test game as skeleton

---
+++ house in fire/Assets/script/charactèrecontroleur.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class charactèrecontroleur : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpForce;
+    [SerializeField] private LayerMask groundlayer;
+    private Rigidbody2D rb;
+
+    private InputAction moveAction;
+
+    private InputAction jumpAction;
+
+    private int nbJumpLeft = 0;
+        
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveAction = InputSystem.actions.FindAction("Move");
+        jumpAction = InputSystem.actions.FindAction("Jump");
+
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if((groundlayer.value &(1<<other.gameObject.layer)) != 0)
+        {
+            nbJumpLeft = 2;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = moveAction.ReadValue<Vector2>();
+        rb.linearVelocityX = move.x * moveSpeed;
+        if (jumpAction.WasPressedThisFrame()&& nbJumpLeft>0)
+        {
+            rb.AddForceY(jumpForce);
+            nbJumpLeft--;
+        }
+
+
+    }
+}
