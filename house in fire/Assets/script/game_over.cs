49db06255459056f1ea2cc718f01a94ecb603c6d
Sébastien <morelsebastien@bluewin.ch>
2025-08-08 10:27:17 +0200

    Tried to fix the game over scene not working

--- house in fire/Assets/script/game_over.cs
+++ house in fire/Assets/script/game_over.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    private InputAction restart;
+
+    void Start()
+    {
+        restart = InputSystem.actions.FindAction("Restart");
+    }
+
     // Update is called once per frame
     void Update()
     {
+        if (restart.WasPressedThisFrame())
+        {
+            print("RestartAction was pressed");
+            SceneManager.LoadScene("SampleScene");
+            print("Scene have been changed");
+        }
+
+        /*
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            Debug.Log("Space has been pressed");
             SceneManager.LoadScene("SampleScene");
-        }
+            print("The scene changement worked");
+        }*/
     }
 }
