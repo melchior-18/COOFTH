34606f32c347081f0b1691d14a87372c15c7a5d1
Melchior <melc18hior@gmail.com>
2025-08-08 11:10:15 +0200

    added a menu and start mode

---
+++ house in fire/Assets/script/Menu.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public InputAction start;
+
+    void Start()
+    {
+        start= InputSystem.actions.FindAction("Restart");
+    }
+    
+
+    void OnEnable()
+    {
+        start.Enable();
+    }
+
+    void OnDisable()
+    {
+        start.Disable();
+    }
+
+    void Update()
+    {
+        if (start.WasPressedThisFrame())
+        {
+            SceneManager.LoadScene("SampleScene");
+            Debug.Log("Scene change worked");
+        }
+    }
+}
